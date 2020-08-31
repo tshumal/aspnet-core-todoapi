bff93a251fa61b5203dd3aee356ffbe31bdb966f
Lingani Tshuma <tshumal@gmail.com>
2020-08-31 14:40:59 +0200

    added swagger documentation

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -31,6 +31,9 @@ namespace TodoApi
             services.AddControllers();            
             services.AddDbContext<DataContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<ITodoItemService, TodoItemService>();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,7 +53,17 @@ namespace TodoApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-            });        
+            }); 
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger(); 
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoAPI V1");
+            });      
         }
     }
 }
